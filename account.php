fb1946984b0976f8897242d93387235a5703ab71
divinity76 <divinity76@gmail.com>
2016-04-04 00:23:46 +0200

    $_SESSION['ip'] may be undefined. default to 0.

--- account.php
+++ account.php
@@ -5,7 +5,7 @@ include ("functions.php");
 /* Session security */
 if (isset($_GET['logout']) || 
 ((time()-($_SESSION['last_activity']??0))>$cfg['session_timeout'] && $cfg['session_security'] > 0) || 
-($_SERVER['REMOTE_ADDR'] != $_SESSION['ip'] && $cfg['session_security'] > 1)){
+($_SERVER['REMOTE_ADDR'] != ($_SESSION['ip']??'0') && $cfg['session_security'] > 1)){
 	$_SESSION['account']='';
 	$_SESSION['access']='';
 }
